996631038585209184291623ae5ad34169f6b0e9
Anisha Prajapati <121660716+Anisha0911@users.noreply.github.com>
2023-09-11 00:40:19 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+function f1(){
+    const x= document.getElementById("bass_drum");
+    x.play();
+}
+function f2(){
+    const x= document.getElementById("cymbal");
+    x.play();
+}function f3(){
+    const x= document.getElementById("bongo");
+    x.play();
+}function f4(){
+    const x= document.getElementById("bass_guitar");
+    x.play();
+}function f5(){
+    const x= document.getElementById("trumpet");
+    x.play();
+}function f6(){
+    const x= document.getElementById("harmonica");
+    x.play();
+}function f7(){
+    const x= document.getElementById("guitar_stroke");
+    x.play();
+}function f8(){
+    const x= document.getElementById("snare");
+    x.play();
+}function f9(){
+    const x= document.getElementById("piano");
+    x.play();
+}
+function f10(){
+    const x= document.getElementById("harp");
+    x.play();
+}
